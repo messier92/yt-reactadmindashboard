c4f627ecd7aade9a3b473371e8314e4d3eafa6b2
Han Long Eugene, Goh <hanlong2102@hotmail.com>
2023-10-17 11:38:16 +0800

    3.18

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kan
 import {useStateContext} from './contexts/ContextProvider';
 
 const App = () => {
-  const {activeMenu} = useStateContext();
+  const {activeMenu, themeSettings, setThemeSettings} = useStateContext();
   
   return (
     <div>
@@ -17,7 +17,7 @@ const App = () => {
         <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
             <TooltipComponent content="Settings" position="Top">
                     <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                    style={{ background: 'blue', borderRadius: '50%'}}>
+                    onClick={()=>setThemeSettings(true)} style={{ background: 'blue', borderRadius: '50%'}}>
                     <FiSettings/>
                     </button>
             </TooltipComponent>
@@ -33,9 +33,9 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar/>
             </div>
-          </div>
-
+   
           <div>
+            {themeSettings && <ThemeSettings/>}
             <Routes>
               {/* Dashboard */}
               <Route path="/" element={<Ecommerce/>}/>
@@ -60,6 +60,8 @@ const App = () => {
               <Route path="/stacked" element={<Stacked/>}/>
             </Routes>
           </div>
+          </div>
+
         </div>
     </BrowserRouter>
     </div>
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -11,5 +11,5 @@ export {default as ChartsHeader} from './ChartsHeader';
 export {default as Header} from './Header';
 export {default as SparkLine} from './Charts/SparkLine';
 export {default as LineChart} from './Charts/LineChart';
-export {default as Stakced} from './Charts/Stacked';
+export {default as Stacked} from './Charts/Stacked';
 export {default as Pie} from './Charts/Pie';
